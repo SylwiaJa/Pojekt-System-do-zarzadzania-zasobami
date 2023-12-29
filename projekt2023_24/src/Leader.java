9d4e0caa4a706353dd78acfe065d98be141f2090
Grzegorz Listwan <listwan94@gmail.com>
2023-12-29 12:44:05 +0100

    dodano klasę Leader

---
+++ projekt2023_24/src/Leader.java
@@ -0,0 +1,38 @@
+import java.util.List;
+
+public class Leader extends Employee {
+    public Leader(String id, String name, String lastName, String role, String zone, String login, String password) {
+        super(id, name, lastName, role, zone, login, password);
+    }
+
+    public Leader(String id, String name, String lastName, String role, String zone, String login, String password, Task task) {
+        super(id, name, lastName, role, zone, login, password, task);
+    }
+
+    @Override
+    public List<Task> getListOfTasks() {
+        return null;
+    }
+
+    public List<Employee> getListOfEmployeeAndTask() {
+
+    }
+
+    public void getTimeReport() {
+
+    }
+
+    public void getZoneReport() {
+
+    }
+public void getEmployeeReport(){
+
+}
+public void getEquipmentStatusReport(){
+
+}
+public void getEquipmentTimeOfUseReport(){
+
+}
+
+}
