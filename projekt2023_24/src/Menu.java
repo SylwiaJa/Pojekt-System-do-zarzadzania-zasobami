71b437212f28d10e1ea57b2f7a9a667925a140be
Grzegorz Listwan <listwan94@gmail.com>
2023-12-29 13:44:29 +0100

    dodano klasę Menu

---
+++ projekt2023_24/src/Menu.java
@@ -0,0 +1,13 @@
+public class Menu {
+    public void displayLoginMenu(){};
+    public void login(String login, String password){};
+    public void displayMenuAfterLogin(Employee employee){};
+    public void loginErrorInfo(){};
+    public void logOut(Employee employee){};
+    public void logOutInfo(){};
+    public void displayMenuWorker(Employee employee){};
+    public void displayMenuLeader(Employee employee){};
+    public void displayMenuManager(Employee employee){};
+    public void displayMenuAdmin(Employee employee){};
+    public void updateListOfTask(){};
+}
