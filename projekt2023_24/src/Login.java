ba2d9fd988febd4d4d561b8d166c35bb7933394e
Grzegorz Listwan <listwan94@gmail.com>
2023-12-29 13:39:15 +0100

    dodano klasę Login

---
+++ projekt2023_24/src/Login.java
@@ -0,0 +1,11 @@
+public class Login {
+    public boolean check(String login, String password){
+        return true;
+    }
+    public void  startLogin(Employee employee){
+
+    }
+    public void endLogin(Employee employee){
+
+    }
+}
